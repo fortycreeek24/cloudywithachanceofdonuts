e608c38a34da63f1070078c5a62532bec2709bb8
Noah Smith <fortycreeek24@yahoo.com>
2019-11-15 11:06:33 -0600

    adds routes

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 const express = require ('express');
+
 const app = express();
 
 
 const port = 3000;
 
+//donut routes
 
 app.get('/', (req, res) => {
     res.send('Hello World')
     });
+
+
+//CREATE
+//ADD A NEW DONUT
+app.post('/donuts',(req,res) =>{
+    //gets an object via req body
+    res.send('placeholder')
+});
+
+// GET ALL DONUTS
+app.get('/donuts', (req,res) =>{
+    res.send('no donuts yet, my b');
+});
+
+//GET SINGLE DONUT RECORD
+app.get('/donuts/:id', (req,res) =>{
+    res.send('make single donut')
+});
+
+//DELETE DONUT
+app.delete('/donuts/:id', (req, res)=>{
+    //req.body
+    res.send('sell a single donut')
+});
+
+//UPDATE A DONUT
+app.patch('/donuts/:id',(req,res) =>{
+    //req.body
+    res.send('made new donut')
+});
+
+//listen on port
+
 
 
 app.listen(port, () => {

ef1f44c577d01cb126d0a110b781a2461d23e3d3
Noah Smith <fortycreeek24@yahoo.com>
2019-11-15 12:13:36 -0600

    adds donuts to table

---
+++ seed.sql
@@ -0,0 +1,9 @@
+TRUNCATE donuts; 
+
+INSERT INTO donuts(filling, stale) VALUES ('blueberry',0);
+INSERT INTO donuts(filling, stale) VALUES ('strawberry',0);
+INSERT INTO donuts(filling, stale) VALUES ('cream',0);
+INSERT INTO donuts(filling, stale) VALUES ('nutella',0);
+INSERT INTO donuts(filling, stale) VALUES ('toothpasta',1);
+INSERT INTO donuts(filling, stale) VALUES ('chili lime',1);
+
